9b6b3c3220a21129c86a3d33756c48be9e7a52d9
Kristian Malmkvist Eie <kmeie1234@gmail.com>
2014-03-15 23:36:22 +0100

    deleting files

--- src/Persons.java
+++ src/Persons.java
@@ -5,17 +5,7 @@ public class Persons {
 	String address;
 	int age;
 	
-	int birthyear(int age){
-		int birthyear = 2014-age;
-		return birthyear;
-	}
-	
-	String tull(String name){
-		String tull = "heisann";
-		String tot = tull + name;
-		return tot;
-	}
-	
+
 	
 	
 	
