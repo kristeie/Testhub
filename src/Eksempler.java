c36ea7573d277765e66948afd9b5a746b6b84c8c
Kristian Malmkvist Eie <kmeie1234@gmail.com>
2014-03-15 22:38:25 +0100

    second commit

---
+++ src/Eksempler.java
@@ -0,0 +1,16 @@
+
+public class Eksempler {
+
+	public static void main(String[] args) {
+		String output = MyComparisons.My 
+		System.out.println("This is my first output, Hello World!");
+		 String first = "a";
+		 String second = "word";
+		 String third = "does";
+		 String fourth = "count";
+		 String concatenate = first +" " + second + " " + third + " " + fourth;
+		 System.out.println(concatenate);
+	}
+
+}
+
---
+++ src/MyComparisons.java
@@ -0,0 +1,14 @@
+
+public class MyComparisons {
+	String first = "This";
+	String second ="is";
+	String third ="a string concatenation";
+	String fourth = "chairs";
+	String fifth = "chairs";
+	
+	void myMethod(String a, String b){
+		if (fourth==fifth){
+			System.out.println("fouth"=="fifth");
+		}
+	}
+}
