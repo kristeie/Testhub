1ee3388ad1b20151c25c905f618921a1ea33449e
Kristian Malmkvist Eie <kmeie1234@gmail.com>
2014-03-15 23:02:52 +0100

    persons

--- src/Eksempler.java
+++ src/Eksempler.java
@@ -2,7 +2,6 @@
 public class Eksempler {
 
 	public static void main(String[] args) {
-		String output = MyComparisons.My 
 		System.out.println("This is my first output, Hello World!");
 		 String first = "a";
 		 String second = "word";
@@ -10,6 +9,11 @@ public class Eksempler {
 		 String fourth = "count";
 		 String concatenate = first +" " + second + " " + third + " " + fourth;
 		 System.out.println(concatenate);
+	}
+	
+	public int add(int a, int b){
+		int c = a + b;
+		return c;
 	}
 
 }
---
+++ src/Persons.java
@@ -0,0 +1,18 @@
+
+public class Persons {
+
+	String name;
+	String address;
+	int age;
+	
+	int birthyear(int age){
+		int birthyear = 2014-age;
+		return birthyear;
+	}
+	
+	
+	
+	
+	
+	
+}
